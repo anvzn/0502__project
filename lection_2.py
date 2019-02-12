fb30a697ee122cc1e177aa46260675d4dd4cb7a7
“Anastasiia” <anastasiavozniza@gmail.com>
2019-02-12 21:34:14 +0200

    5 number ssn added

--- lection_2.py
+++ lection_2.py
@@ -3,7 +3,7 @@ ssn_white_list=[1234, 5678, 9123]
 while True:
     user_ssn_number=input('Enter your SSN number in format (0000): ')
     #check the length
-    if len(user_ssn_number)!=4:
+    if len(user_ssn_number)not in (4,5):
         print('Invalid SSN, please check the format (0000)!!!')
         continue
 
@@ -13,5 +13,6 @@ while True:
 
             if int(user_ssn_number) not in ssn_white_list:
                 print('Your permissions are prohibited, please contact your manager!')
+
 
 
