248216e1de6183ffae030bd26359488e671943ab
“Anastasiia” <anastasiavozniza@gmail.com>
2019-02-12 21:25:45 +0200

    ssn checker added

--- lection_2.py
+++ lection_2.py
@@ -1,7 +1,17 @@
-SSN_white_list=[1234, 5678, 9123]
+ssn_white_list=[1234, 5678, 9123]
 
 while True:
     user_ssn_number=input('Enter your SSN number in format (0000): ')
+    #check the length
     if len(user_ssn_number)!=4:
         print('Invalid SSN, please check the format (0000)!!!')
         continue
+
+        if not user_ssn_number.isdigit():
+            print("Please enter digit only!!!")
+            continue
+
+            if int(user_ssn_number) not in ssn_white_list:
+                print('Your permissions are prohibited, please contact your manager!')
+
+
